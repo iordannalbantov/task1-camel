0dc3d2955d1f5d71f34187bae65fdea6a92b9faa
Yordan Nalbantov <iordannalbantov@gmail.com>
2016-06-09 18:17:23 +0300

    routes and it is deploying with help from Angelo

--- bankx/src/main/java/com/estafet/bench/yordan/nalbantov/task1/camel/routes/BankXRouteBuilder.java
+++ bankx/src/main/java/com/estafet/bench/yordan/nalbantov/task1/camel/routes/BankXRouteBuilder.java
@@ -1,5 +1,6 @@
 package com.estafet.bench.yordan.nalbantov.task1.camel.routes;
 
+import com.estafet.bench.yordan.nalbantov.task1.camel.model.IbanSingleReportEntity;
 import org.apache.camel.builder.RouteBuilder;
 
 import java.time.LocalDateTime;
@@ -14,29 +15,40 @@ public class BankXRouteBuilder extends RouteBuilder {
 
         LocalDateTime now = LocalDateTime.now();
 
-        from("jetty:http://localhost:20616/estafet/iban/report?httpClient.method=POST").id("entry")
-                .split(body()).setHeader("IbanTimestampOfRequest", constant(now))
+        from("jetty:http://localhost:20616/estafet/iban/report?httpClient.method=POST").routeId("entry")
+                .split(body()).setHeader("IbanTimestampOfRequest", /* to string */ constant(now))
                 .to("activemq:queue:ibanReport");
 
-        from("activemq:queue:ibanReport").id("processing")
-                .to("file:///u01/data/iban/reports");
-
 //        from("activemq:queue:ibanReport").id("processing")
-//                .process(exchange -> {
-//                    String iban = exchange.getIn().getBody(String.class);
-//                    IbanSingleReportEntity entity = new IbanSingleReportEntity(iban);
-//                    exchange.getIn().setBody(entity);
-//                })
-//                .enrich("direct:data", (original, resource) -> {
-//                    IbanSingleReportEntity originalBody = original.getIn().getBody(IbanSingleReportEntity.class);
-//
-//                    originalBody.setName("Yordan Nalbantov");
-//                    originalBody.setBalance(100.0);
-//                    originalBody.setCurrency("BGN");
-//
-//                    original.getIn().setBody(originalBody);
-//                    return original;
-//                })
 //                .to("file:///u01/data/iban/reports");
+
+        from("direct:data").process(exchange -> {
+            IbanSingleReportEntity entity = new IbanSingleReportEntity(null);
+
+            entity.setName("Yordan Nalbantov");
+            entity.setBalance(100.0);
+            entity.setCurrency("BGN");
+
+            exchange.getIn().setBody(entity);
+        });
+
+        from("activemq:queue:ibanReport").routeId("processing")
+                .process(exchange -> {
+                    String iban = exchange.getIn().getBody(String.class);
+                    IbanSingleReportEntity entity = new IbanSingleReportEntity(iban);
+                    exchange.getIn().setBody(entity);
+                })
+                .enrich("direct:data", (original, resource) -> {
+                    IbanSingleReportEntity originalBody = original.getIn().getBody(IbanSingleReportEntity.class);
+                    IbanSingleReportEntity resourceBody = resource.getIn().getBody(IbanSingleReportEntity.class);
+
+                    originalBody.setName(resourceBody.getName());
+                    originalBody.setBalance(resourceBody.getBalance());
+                    originalBody.setCurrency(resourceBody.getCurrency());
+
+                    original.getIn().setBody(originalBody);
+                    return original;
+                })
+                .to("file:///u01/data/iban/reports");
     }
 }
